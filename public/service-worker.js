1ca2d742939a921abe6951f8af50dd43aff1c0c1
Chloe <gtalk.with.nani@gmail.com>
2024-01-22 14:53:48 +0900

    service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,9 @@ const CACHE_NAME = 'memorial-data';
 
 self.addEventListener('fetch', (event) => {
   if (
-    event.request.url.includes('/api/articleNews') ||
-    event.request.url.includes('/api/pages') ||
-    event.request.url.includes('/api/editorial') ||
-    event.request.url.includes('/api/watchNews') ||
-    event.request.url.includes('/api/youtubeNews')
+    event.request.url.includes('/api/interviews') ||
+    event.request.url.includes('/api/newsics') ||
+    event.request.url.includes('/api/ebenums')
   ) {
     event.respondWith(
       caches.open(CACHE_NAME).then((cache) => {
