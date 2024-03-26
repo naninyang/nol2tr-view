85a247c858f00c646d87b653464e7e85aaacdfa9
Chloe <gtalk.with.nani@gmail.com>
2024-03-26 12:26:15 +0900

    Sitemap Data bug fixed

--- pages/api/sitemapNewsic.ts
+++ pages/api/sitemapNewsic.ts
@@ -1,16 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-
-const formatDate = (datetime: string) => {
-  const date = new Date(datetime);
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
-
-  return `${year}${month}${day}${hours}${minutes}${seconds}`;
-};
+import { formatDate } from '@/utils/strapi';
 
 async function fetchAllNewsicData() {
   let response = await fetch(
