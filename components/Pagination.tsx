5fade7ce89f0ec2f6bfc9a05c098a1542e0e6628
Chloe <gtalk.with.nani@gmail.com>
2024-06-09 18:44:20 +0900

    Update Pagination.tsx

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -23,71 +23,75 @@ export function Pagination({
   const nextGroupPage = endPage + 1;
 
   return (
-    <div className={styles.pagination}>
-      {currentGroup > 0 && (
-        <div className={styles.pager}>
-          {sorting === 'interviews' ? (
-            <Anchor href={`/interviews?category=${category}&page=${prevGroupPage}`}>
-              <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M5 2.46484L1.46484 6L5 9.53516L5.75 8.78516L3.46484 6.5H10.5V5.5H3.46484L5.75 3.21484L5 2.46484Z"
-                  fill="black"
-                />
-              </svg>
-              <span>이전</span>
-            </Anchor>
-          ) : (
-            <Anchor href={`/newsics?page=${prevGroupPage}`}>
-              <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M5 2.46484L1.46484 6L5 9.53516L5.75 8.78516L3.46484 6.5H10.5V5.5H3.46484L5.75 3.21484L5 2.46484Z"
-                  fill="black"
-                />
-              </svg>
-              <span>이전</span>
-            </Anchor>
-          )}
-        </div>
-      )}
-      {Array.from({ length: endPage - startPage + 1 }).map((_, i) => {
-        const pageNumber = startPage + i;
-        return (
-          <React.Fragment key={pageNumber}>
-            <div className={`${styles.pages} ${pageNumber === currentPage ? styles.current : ''}`}>
+    <>
+      {endPage > startPage && (
+        <div className={styles.pagination}>
+          {currentGroup > 0 && (
+            <div className={styles.pager}>
               {sorting === 'interviews' ? (
-                <Anchor href={`/interviews?category=${category}&page=${pageNumber}`}>{pageNumber}</Anchor>
+                <Anchor href={`/interviews?category=${category}&page=${prevGroupPage}`}>
+                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M5 2.46484L1.46484 6L5 9.53516L5.75 8.78516L3.46484 6.5H10.5V5.5H3.46484L5.75 3.21484L5 2.46484Z"
+                      fill="black"
+                    />
+                  </svg>
+                  <span>이전</span>
+                </Anchor>
               ) : (
-                <Anchor href={`/newsics?page=${pageNumber}`}>{pageNumber}</Anchor>
+                <Anchor href={`/newsics?page=${prevGroupPage}`}>
+                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M5 2.46484L1.46484 6L5 9.53516L5.75 8.78516L3.46484 6.5H10.5V5.5H3.46484L5.75 3.21484L5 2.46484Z"
+                      fill="black"
+                    />
+                  </svg>
+                  <span>이전</span>
+                </Anchor>
               )}
             </div>
-          </React.Fragment>
-        );
-      })}
-      {currentGroup < Math.ceil(pageCount / pageLimit) - 1 && (
-        <div className={styles.pager}>
-          {sorting === 'interviews' ? (
-            <Anchor href={`/interviews?category=${category}&page=${nextGroupPage}`}>
-              <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M7 2.46484L6.25 3.21484L8.53516 5.5H1.5V6.5H8.53516L6.25 8.78516L7 9.53516L10.5352 6L7 2.46484Z"
-                  fill="black"
-                />
-              </svg>
-              <span>다음</span>
-            </Anchor>
-          ) : (
-            <Anchor href={`/newsics?page=${nextGroupPage}`}>
-              <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M7 2.46484L6.25 3.21484L8.53516 5.5H1.5V6.5H8.53516L6.25 8.78516L7 9.53516L10.5352 6L7 2.46484Z"
-                  fill="black"
-                />
-              </svg>
-              <span>다음</span>
-            </Anchor>
+          )}
+          {Array.from({ length: endPage - startPage + 1 }).map((_, i) => {
+            const pageNumber = startPage + i;
+            return (
+              <React.Fragment key={pageNumber}>
+                <div className={`${styles.pages} ${pageNumber === currentPage ? styles.current : ''}`}>
+                  {sorting === 'interviews' ? (
+                    <Anchor href={`/interviews?category=${category}&page=${pageNumber}`}>{pageNumber}</Anchor>
+                  ) : (
+                    <Anchor href={`/newsics?page=${pageNumber}`}>{pageNumber}</Anchor>
+                  )}
+                </div>
+              </React.Fragment>
+            );
+          })}
+          {currentGroup < Math.ceil(pageCount / pageLimit) - 1 && (
+            <div className={styles.pager}>
+              {sorting === 'interviews' ? (
+                <Anchor href={`/interviews?category=${category}&page=${nextGroupPage}`}>
+                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M7 2.46484L6.25 3.21484L8.53516 5.5H1.5V6.5H8.53516L6.25 8.78516L7 9.53516L10.5352 6L7 2.46484Z"
+                      fill="black"
+                    />
+                  </svg>
+                  <span>다음</span>
+                </Anchor>
+              ) : (
+                <Anchor href={`/newsics?page=${nextGroupPage}`}>
+                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M7 2.46484L6.25 3.21484L8.53516 5.5H1.5V6.5H8.53516L6.25 8.78516L7 9.53516L10.5352 6L7 2.46484Z"
+                      fill="black"
+                    />
+                  </svg>
+                  <span>다음</span>
+                </Anchor>
+              )}
+            </div>
           )}
         </div>
       )}
-    </div>
+    </>
   );
 }
