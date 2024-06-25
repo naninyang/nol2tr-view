7686922ddc2394024c192a500b727bafa1e87cef
Chloe <gtalk.with.nani@gmail.com>
2024-06-26 07:08:18 +0900

    Home page Data fetching fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -461,9 +461,9 @@ export default function Home({ banner, bannerError }: { banner: any; interviews:
               </div>
               {!newsicLoading ? (
                 <>
-                  {interviewData && Array.isArray(interviewData.articles) && (
+                  {newsicData && Array.isArray(newsicData.articles) && (
                     <div className={styles['article-list']}>
-                      {interviewData.articles.map((article: NewsData) => (
+                      {newsicData.articles.map((article: NewsData) => (
                         <article key={article.idx}>
                           <Link key={article.idx} href={`/interview/${article.idx}`} scroll={true} shallow={true}>
                             {article.platform === 'youtube' ? (
@@ -473,16 +473,6 @@ export default function Home({ banner, bannerError }: { banner: any; interviews:
                             )}
                             <h3>{article.subject}</h3>
                             <p className={styles.summary}>{article.summary}</p>
-                            <dl className={styles.people}>
-                              <div>
-                                <dt>인터뷰어</dt>
-                                <dd>{article.interviewer}</dd>
-                              </div>
-                              <div>
-                                <dt>인터뷰이</dt>
-                                <dd>{article.interviewee}</dd>
-                              </div>
-                            </dl>
                             <dl className={styles.recommended}>
                               <dt>
                                 <i />
